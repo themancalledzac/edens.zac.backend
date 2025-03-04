e50488b293a6c1b19bf05d8dfdd5e85c113d9fc2
Zac Edens <edens.zac@gmail.com>
2025-03-03 16:55:30 -0800

    0004 - GetBlog now working. updated a bit of imageServiceImpl, removed old code. Also new method in ImageProcessingUtil that verifies the image is cloudfront and not s3

--- src/main/java/edens/zac/portfolio/backend/entity/BlogEntity.java
+++ src/main/java/edens/zac/portfolio/backend/entity/BlogEntity.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
--- src/main/java/edens/zac/portfolio/backend/repository/BlogRepository.java
+++ src/main/java/edens/zac/portfolio/backend/repository/BlogRepository.java
@@ -2,11 +2,20 @@ package edens.zac.portfolio.backend.repository;
 
 import edens.zac.portfolio.backend.entity.BlogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BlogRepository extends JpaRepository<BlogEntity, Long> {
 
     BlogEntity save(BlogEntity blog);
 
+    @Query("SELECT b FROM BlogEntity b LEFT JOIN FETCH b.images WHERE b.id = :id")
+    Optional<BlogEntity> findByIdWithImages(@Param("id") Long id);
+
+    @Query("SELECT b FROM BlogEntity b WHERE b.id = :id")
+    Optional<BlogEntity> findById(@Param("id") Long id);
 }
