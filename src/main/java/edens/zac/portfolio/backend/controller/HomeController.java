d0d2f1fb7f138ffda4ed7cb5294b258635c21ac6
Zac Edens <edens.zac@gmail.com>
2025-03-08 16:34:55 -0800

    0009 - let's push this branch

--- src/main/java/edens/zac/portfolio/backend/controller/HomeController.java
+++ src/main/java/edens/zac/portfolio/backend/controller/HomeController.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +38,4 @@ public class HomeController {
                     .body("Failed to retrieve catalog: " + e.getMessage());
         }
     }
-
-
-
 }
