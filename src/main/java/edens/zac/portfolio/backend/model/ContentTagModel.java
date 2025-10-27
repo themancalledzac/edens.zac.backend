a53c035985fe7ed2d56bf91878756f68973d0867
themancalledzac <themancalledzac@gmail.com>
2025-10-26 18:07:18 -0700

    0043-update-edit-page-part-4

--- src/main/java/edens/zac/portfolio/backend/model/ContentTagModel.java
+++ src/main/java/edens/zac/portfolio/backend/model/ContentTagModel.java
@@ -5,12 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Model representing a content tag for API responses.
- * Contains the tag's ID, name, and related entity IDs.
+ * Contains the tag's ID and name.
  */
 @Data
 @NoArgsConstructor
@@ -20,22 +17,4 @@ public class ContentTagModel {
 
     private Long id;
     private String name;
-
-    /**
-     * List of content collection IDs that use this tag.
-     */
-    @Builder.Default
-    private List<Long> contentCollectionIds = new ArrayList<>();
-
-    /**
-     * List of image content block IDs that use this tag.
-     */
-    @Builder.Default
-    private List<Long> imageContentBlockIds = new ArrayList<>();
-
-    /**
-     * List of gif content block IDs that use this tag.
-     */
-    @Builder.Default
-    private List<Long> gifContentBlockIds = new ArrayList<>();
 }
